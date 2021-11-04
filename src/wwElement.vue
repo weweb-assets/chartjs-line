0ba99a130af1149f6ba4650e59ada1a6cf42b347
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-11-04 16:26:26 +0100

    clean for PR

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,95 +1,22 @@
 <template>
-    <div class="chart-container" style="position: relative">
-        <canvas class="chartjs-line"></canvas>
-    </div>
+    <div class="chart-container"></div>
 </template>
 
 <script>
-import { Chart, registerables } from 'chart.js';
-Chart.register(...registerables);
-
 export default {
     props: {
         content: { type: Object, required: true },
     },
-    data() {
-        this.chartInstance = null;
-        return this.chartInstance;
-    },
-    computed: {
-        config() {
-            return {
-                type: 'line',
-                data: {
-                    labels: this.content.labels,
-                    datasets: this.content.datasets,
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    plugins: {
-                        legend: {
-                            position: this.content.legendPosition,
-                            labels: { usePointStyle: true },
-                        },
-                    },
-                },
-            };
-        },
-    },
-    watch: {
-        config() {
-            if (
-                this.chartInstance &&
-                this.content.labels &&
-                this.content.labels.length &&
-                this.content.datasets &&
-                this.content.datasets.length
-            ) {
-                this.chartInstance.data.labels = this.content.labels;
-                this.chartInstance.data.datasets = this.content.datasets;
-                this.chartInstance.update();
-            }
-        },
-        'content.legendPosition'() {
-            this.chartInstance.options.plugins.legend.position = this.content.legendPosition;
-            this.chartInstance.update();
-        },
-        'content.legendAlignement'() {
-            this.chartInstance.options.plugins.legend.align = this.content.legendAlignement;
-            this.chartInstance.update();
-        },
-        'content.axis'() {
-            if (this.chartInstance) this.chartInstance.destroy();
-            this.initChart();
-        },
-        'content.stacked'() {
-            if (this.chartInstance) this.chartInstance.destroy();
-            this.initChart();
-        },
-    },
-    mounted() {
-        this.initChart();
-    },
-    beforeUnmount() {
-        this.chartInstance.destroy();
-    },
-    methods: {
-        initChart() {
-            const element = this.$el.querySelector('.chartjs-line');
-            this.chartInstance = new Chart(element, this.config);
-        },
-    },
+    data() {},
+    computed: {},
+    watch: {},
+    mounted() {},
+    beforeUnmount() {},
+    methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
 .chart-container {
-    width: inherit;
-    height: inherit;
-    #chartjs-pie {
-        width: 100% !important;
-        height: 100% !important;
-    }
 }
 </style>
